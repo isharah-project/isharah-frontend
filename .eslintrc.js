713169198c976157f84e94ba19d5bbd1c9f9d995
tarekalqaddy <tarek.alqaddy@gmail.com>
2019-02-21 03:00:22 +0200

    Init nuxt project.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs'
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/html-self-closing': 0,
+    'vue/attribute-hyphenation': 0,
+    'prefer-const': 0,
+    'no-console': 0
+  }
+}
