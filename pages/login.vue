8a1c06d14022e9a3885dfcf3139e133d9075d9d9
miralelnahas <miralelnahas@gmail.com>
2019-03-19 23:56:01 +0200

    validations added

--- pages/login.vue
+++ pages/login.vue
@@ -6,36 +6,74 @@
         <v-card v-bind:class="{ [`elevation-${8}`]: true }" class="py-4" >
           <v-card-text v-if="signin">تسجيل الدخول</v-card-text>
           <v-card-text v-else>إنشاء حساب</v-card-text>
-          <v-form class="mx-5">
-            <v-text-field required label="البريد الالكتروني" :rules="emailRules"></v-text-field>
-            <v-text-field required label="كلمة السر" type="password" :rules="textRules"></v-text-field>
-            <v-text-field required label = "تأكيد كلمة السر" :rules="textRules" v-if="!signin" type = "password"></v-text-field>
-            <v-text-field label = "اسم المستخدم" :rules="textRules" v-if="!signin"></v-text-field>
-            <v-text-field label = "رقم التليفون" v-if="!signin" type= "number"></v-text-field>
-            <v-text-field label = "تاريخ الميلاد" v-if="!signin" type = "date" value = "2000-12-30"></v-text-field>
+          <v-form class="mx-5" v-if="signin" @submit.prevent = "login" ref="loginForm">
+            <v-text-field label="البريد الالكتروني" v-model="email" :rules="emailRules"></v-text-field>
+            <v-text-field label="كلمة السر" type="password" v-model="password" :rules="textRules"></v-text-field>
+            <v-btn
+              :ripple="false"
+              dark
+              flat
+              round
+              class="blue-cyan-gradient fixed-size-btn"
+              type="submit"
+              v-if="signin"
+            >
+              دخول
+            </v-btn>
           </v-form>
-          <v-btn
-            :ripple="false"
-            dark
-            flat
-            round
-            class="blue-cyan-gradient fixed-size-btn"
-            @click="login"
-            v-if="signin"
-          >
-            دخول
-          </v-btn>
-          <v-btn
-            :ripple="false"
-            dark
-            flat
-            round
-            class="blue-cyan-gradient fixed-size-btn"
-            @click="register"
-            v-else
-          >
-            انشاء
-          </v-btn>
+          <v-form class="mx-5" lazy-validation v-else @submit.prevent = "register" ref="signupForm">
+            <v-text-field label="البريد الالكتروني" v-model="email" :rules="emailRules"></v-text-field>
+            <v-text-field
+              label="كلمة السر"
+              type="password"
+              v-model="password"
+              :rules="textRules"></v-text-field>
+            <v-text-field
+              label="تأكيد كلمة السر"
+              type="password"
+              v-model="password_confirmation"
+              :error-messages=" this.password_confirmation !== null && this.password !== null && this.password === this.password_confirmation ? [] : ['كلمة السر و تأكيد كلمة السر غير متطابقين']"
+              :rules="textRules"></v-text-field>
+            <v-text-field label = "الأسم الأول" v-model="first_name" :rules="textRules" ></v-text-field>
+            <v-text-field label = "الأسم الأخير" v-model="last_name" :rules="textRules" ></v-text-field>
+            <v-select label = "النوع"  :items= "gender_list" :rules="textRules"></v-select>
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              lazy
+              transition="scale-transition"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="date_of_birth"
+                  label="تاريخ الميلاد"
+                  prepend-icon="event"
+                  readonly
+                  v-on="on"
+                  :rules="textRules"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                ref="picker"
+                v-model="date_of_birth"
+                :max="new Date().toISOString().substr(0, 10)"
+                min="1950-01-01"
+                @change="save"
+              ></v-date-picker>
+            </v-menu>
+            <v-btn
+              :ripple="false"
+              dark
+              flat
+              round
+              class="blue-cyan-gradient fixed-size-btn"
+              type="submit"
+            >
+              إنشاء
+            </v-btn>
+          </v-form>
           <v-card-text v-if="signin">
             ليس لديك حساب؟
             <v-btn @click="signup" flat class="blue--text">
@@ -54,29 +92,6 @@
       </v-flex>
     </v-layout>
   </v-container>
-    <!-- <v-btn @click="register">
-      Register
-    </v-btn>
-    <v-btn
-      :ripple="false"
-      dark
-      flat
-      round
-      class="blue-cyan-gradient"
-      @click="login"
-    >
-      Login
-    </v-btn>
-    <v-btn
-      :ripple="false"
-      dark
-      flat
-      round
-      class="red-gradient fixed-size-btn"
-      @click="logout"
-    >
-      Logout
-    </v-btn> -->
 </template>
 
 <script>
@@ -90,38 +105,70 @@ export default {
       ],
       textRules: [
         v => !!v || 'الخانة مطلوبة'
-      ]
+      ],
+      email: '',
+      password: '',
+      password_confirmation: '',
+      first_name: '',
+      last_name: '',
+      gender: '',
+      city: '',
+      country: '',
+      date_of_birth: '',
+      gender_list: ['ذكر', 'انثى'],
+      date: null,
+      menu: false
+    }
+  },
+  watch: {
+    menu (val) {
+      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
     }
   },
   methods: {
+    save (date) {
+      this.$refs.menu.save(date)
+    },
     signup () {
       this.signin = false
     },
+    genderSelect () {
+      if (this.gender === 'ذكر') {
+        return 'male'
+      }
+      return 'female'
+    },
     register () {
-      this.$axios.$post('auth', {
-        'email': 'tarek.alqaddy@gmail.com',
-        'password': 'password',
-        'password_confirmation': 'password',
-        'first_name': 'youssef',
-        'last_name': 'fares',
-        'gender': 'male',
-        'city': 'Cairo',
-        'country': 'Egypt',
-        'date_of_birth': '20/03/1996',
-        'confirm_success_url': '/'
-      }).then((r) => {
-        console.log(r)
-      }).catch((e) => {
-        console.log(e)
-      })
+      if (this.$refs.signupForm.validate()) {
+        this.$axios.$post('auth', {
+          'email': this.email,
+          'password': this.password,
+          'password_confirmation': this.password,
+          'first_name': this.first_name,
+          'last_name': this.last_name,
+          'gender': this.genderSelect(),
+          'city': 'Cairo',
+          'country': 'Egypt',
+          'date_of_birth': this.date_of_birth,
+          'confirm_success_url': process.env.FRONTEND_URL + '/login'
+        }).then((r) => {
+          // yro7 page y2olo eno y-check elemail w y-confirm
+        }).catch((e) => {
+          console.log(e)
+        })
+      }
     },
     login () {
-      this.$auth.loginWith('local', {
-        data: {
-          email: 'youssefares@egsl.com',
-          password: 'password'
-        }
-      })
+      if (this.$refs.loginForm.validate()) {
+        this.$auth.loginWith('local', {
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        }).then(
+          this.$router.push({ path: process.env.FRONTEND_URL + '/' })
+        )
+      }
     },
     logout () {
       this.$auth.logout()
