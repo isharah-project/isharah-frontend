8636a0c8f60ca562b5e3432660fec8324b9801dc
tarekalqaddy <tarek.alqaddy@gmail.com>
2019-03-22 16:42:13 +0200

    Add video selector.

--- pages/contribute/upload.vue
+++ pages/contribute/upload.vue
@@ -1,21 +1,57 @@
 <template>
   <v-container>
-    <input ref="fileInput" type="file">
-    <div v-show="isState([states.PLAYBACK, states.UPLOAD])" style="width: 50%">
-      <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered"></video>
-    </div>
-    <div v-show="isState([states.LIVE_PREVIEW, states.RECORD_STARTED])" style="width: 50%">
-      <video ref="livePreview" class="live-preview-video"></video>
-    </div>
-    <v-btn v-if="isState([states.UPLOAD, states.PLAYBACK])" class="primary" @click="requestUserMedia">
-      Record
-    </v-btn>
-    <v-btn v-if="isState(states.LIVE_PREVIEW)" class="primary" @click="startRecording">
-      Start
-    </v-btn>
-    <v-btn v-if="isState(states.RECORD_STARTED)" class="primary" @click="stopRecording">
-      End
-    </v-btn>
+    <PageHeader icon="videocam" text="سجل فيديو" />
+    <v-layout row wrap>
+      <v-flex xs12 class="text-xs-center">
+        <v-btn
+          class="orange-gradient round-corners video-method-btn btn-shadow title mx-2"
+          :class="{ 'btn-active': isState(selectedState, selectedStates.UPLOAD) }"
+          flat
+          dark
+          @click="selectedState = selectedStates.UPLOAD"
+        >
+          إرفع
+        </v-btn>
+        <v-btn
+          class="red-gradient round-corners video-method-btn btn-shadow title mx-2"
+          :class="{ 'btn-active': isState(selectedState, selectedStates.RECORD) }"
+          flat
+          dark
+          @click="selectedState = selectedStates.RECORD"
+        >
+          سجل
+        </v-btn>
+      </v-flex>
+      <v-flex
+        v-if="isState(selectedState, selectedStates.UPLOAD)"
+        class="text-xs-center pa-5"
+        xs12
+      >
+        <p>السحب والإسقاط في اي مكان للتحميل</p>
+        <br>
+        <v-btn class="orange-gradient fixed-size-btn" flat dark round>
+          او اضغط هنا
+        </v-btn>
+      </v-flex>
+      <v-flex xs12>
+        <input ref="fileInput" type="file">
+        <div v-show="isState(videoState, [videoStates.PLAYBACK, videoStates.UPLOAD])" style="width: 50%">
+          <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered"></video>
+        </div>
+        <div v-show="isState(videoState, [videoStates.LIVE_PREVIEW, videoStates.RECORD_STARTED])" style="width: 50%">
+          <video ref="livePreview" class="live-preview-video"></video>
+        </div>
+        <v-btn v-if="isState(videoState, [videoStates.UPLOAD, videoStates.PLAYBACK])" class="primary" @click="requestUserMedia">
+          Record
+        </v-btn>
+        <v-btn v-if="isState(videoState, videoStates.LIVE_PREVIEW)" class="primary" @click="startRecording">
+          Start
+        </v-btn>
+        <v-btn v-if="isState(videoState, videoStates.RECORD_STARTED)" class="primary" @click="stopRecording">
+          End
+        </v-btn>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -24,9 +60,11 @@ import videojs from 'video.js'
 import RecordRTC from 'recordrtc'
 import 'video.js/dist/video-js.css'
 import '~/assets/rangeslider-videojs/rangeslider.css'
+import PageHeader from '~/components/generic/PageHeader'
 // require('~/assets/rangeslider-videojs/rangeslider.js')
 
 export default {
+  components: { PageHeader },
   data () {
     return {
       videojsRef: null,
@@ -34,13 +72,18 @@ export default {
       videoBlob: null,
       stream: null,
       recorder: null,
-      states: {
+      videoStates: {
         UPLOAD: 0,
         PLAYBACK: 1,
         LIVE_PREVIEW: 2,
         RECORD_STARTED: 3
       },
-      currentState: 0,
+      selectedStates: {
+        UPLOAD: 0,
+        RECORD: 1
+      },
+      videoState: 0,
+      selectedState: 0,
       videoOptions: {
         controls: true,
         autoplay: 'auto',
@@ -52,7 +95,7 @@ export default {
     // window.videojs = videojs
     this.initVideoJs(this.$refs.videoPlayer)
     this.addFileInputListener()
-    this.currentState = this.states.UPLOAD
+    this.videoState = this.videoStates.UPLOAD
   },
   methods: {
     initVideoJs (el) {
@@ -70,7 +113,7 @@ export default {
     addFileInputListener () {
       this.$refs.fileInput.onchange = (event) => {
         if (event.target.files && event.target.files[0]) {
-          this.currentState = this.states.PLAYBACK
+          this.videoState = this.videoStates.PLAYBACK
           this.videoBlob = event.target.files[0]
           this.setVideoJsSource(URL.createObjectURL(this.videoBlob), this.videoBlob.type)
         }
@@ -86,7 +129,7 @@ export default {
         this.recorder = RecordRTC(stream, {
           type: 'video'
         })
-        this.currentState = this.states.LIVE_PREVIEW
+        this.videoState = this.videoStates.LIVE_PREVIEW
       }).catch((e) => {
         if (e.name === 'NotAllowedError') {
           // TODO: ask user to accept permission
@@ -96,7 +139,7 @@ export default {
     },
     startRecording () {
       this.recorder.startRecording()
-      this.currentState = this.states.RECORD_STARTED
+      this.videoState = this.videoStates.RECORD_STARTED
     },
     stopRecording () {
       this.recorder.stopRecording(() => {
@@ -104,14 +147,14 @@ export default {
         this.videojsRef.src([
           { src: URL.createObjectURL(this.videoBlob), type: 'video/webm' }
         ])
-        this.currentState = this.states.PLAYBACK
+        this.videoState = this.videoStates.PLAYBACK
       })
     },
-    isState (state) {
-      if (Array.isArray(state)) {
-        return Object.values(state).some(s => s === this.currentState)
+    isState (state, testState) {
+      if (Array.isArray(testState)) {
+        return Object.values(testState).some(s => s === state)
       } else {
-        return this.currentState === state
+        return state === testState
       }
     }
   }
@@ -121,5 +164,12 @@ export default {
 <style>
 .live-preview-video {
   width: 100%;
+}
+.video-method-btn {
+  width: 200px;
+  height: 50px;
+}
+.btn-active {
+  transform: translateY(5px);
 }
 </style>
