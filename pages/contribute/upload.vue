286b133417a527a59a65d088875c65fd94cddb76
tarekalqaddy <tarek.alqaddy@gmail.com>
2019-03-23 04:20:06 +0200

    Refactor video states & add uplad btn.

--- pages/contribute/upload.vue
+++ pages/contribute/upload.vue
@@ -1,55 +1,76 @@
 <template>
   <v-container>
-    <PageHeader icon="videocam" text="سجل فيديو" />
+    <PageHeader icon="videocam" text="سجل فيديو" class="mb-0" />
     <v-layout row wrap>
-      <v-flex xs12 class="text-xs-center">
+      <v-flex xs12 class="text-xs-center mb-4">
         <v-btn
-          class="orange-gradient round-corners video-method-btn btn-shadow title mx-2"
-          :class="{ 'btn-active': isState(selectedState, selectedStates.UPLOAD) }"
+          class="orange-gradient video-method-btn btn-shadow title mx-2"
+          :class="{ 'btn-active': isParentState('UPLOAD') }"
           flat
           dark
-          @click="selectedState = selectedStates.UPLOAD"
+          round
+          @click="state = states.UPLOAD.INIT"
         >
           إرفع
         </v-btn>
         <v-btn
-          class="red-gradient round-corners video-method-btn btn-shadow title mx-2"
-          :class="{ 'btn-active': isState(selectedState, selectedStates.RECORD) }"
+          class="red-gradient video-method-btn btn-shadow title mx-2"
+          :class="{ 'btn-active': isParentState('RECORD') }"
           flat
           dark
-          @click="selectedState = selectedStates.RECORD"
+          round
+          @click="state = states.RECORD.INIT"
         >
           سجل
         </v-btn>
       </v-flex>
-      <v-flex
-        v-if="isState(selectedState, selectedStates.UPLOAD)"
-        class="text-xs-center pa-5"
-        xs12
-      >
-        <p>السحب والإسقاط في اي مكان للتحميل</p>
-        <br>
-        <v-btn class="orange-gradient fixed-size-btn" flat dark round>
-          او اضغط هنا
+      <v-flex v-if="isParentState('UPLOAD')" class="text-xs-center" xs12>
+        <v-btn
+          class="orange-gradient btn-shadow fixed-size-btn upload-btn"
+          :class="{ 'chosen': videoBlob }"
+          flat
+          dark
+          round
+          @click="triggerFileInput"
+        >
+          {{ videoBlob ? videoBlob.name : 'أو اختار الملف' }}
         </v-btn>
       </v-flex>
-      <v-flex xs12>
-        <input ref="fileInput" type="file">
-        <div v-show="isState(videoState, [videoStates.PLAYBACK, videoStates.UPLOAD])" style="width: 50%">
-          <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered"></video>
-        </div>
-        <div v-show="isState(videoState, [videoStates.LIVE_PREVIEW, videoStates.RECORD_STARTED])" style="width: 50%">
-          <video ref="livePreview" class="live-preview-video"></video>
-        </div>
-        <v-btn v-if="isState(videoState, [videoStates.UPLOAD, videoStates.PLAYBACK])" class="primary" @click="requestUserMedia">
-          Record
-        </v-btn>
-        <v-btn v-if="isState(videoState, videoStates.LIVE_PREVIEW)" class="primary" @click="startRecording">
-          Start
-        </v-btn>
-        <v-btn v-if="isState(videoState, videoStates.RECORD_STARTED)" class="primary" @click="stopRecording">
-          End
-        </v-btn>
+      <v-flex
+        v-if="isParentState('UPLOAD')"
+        class="text-xs-center upload-zone round-corners"
+        :class="{ 'shrink': videoBlob }"
+        xs12
+      >
+        <input ref="fileInput" type="file" class="d-none">
+        <p class="ma-0 upload-zone-phrase">
+          السحب والإسقاط في اي مكان للتحميل
+        </p>
+        <br>
+      </v-flex>
+      <v-flex xs12 class="mt-3">
+        <v-layout>
+          <v-flex md7>
+            <div v-show="isState([states.UPLOAD.INIT, states.RECORD.INIT])" class="">
+              <img src="http://placehold.it/1280x720" alt="" class="full-width round-corners">
+            </div>
+            <div v-show="isState([states.UPLOAD.PLAYBACK, states.RECORD.PLAYBACK])">
+              <video ref="videoPlayer" class="video-js vjs-default-skin vjs-big-play-centered"></video>
+            </div>
+            <div v-show="isState([states.RECORD.LIVE_PREVIEW, states.RECORD.RECORDING])">
+              <video ref="livePreview" class="live-preview-video"></video>
+            </div>
+            <v-btn class="primary" @click="requestUserMedia">
+              Record
+            </v-btn>
+            <v-btn class="primary" @click="startRecording">
+              Start
+            </v-btn>
+            <v-btn class="primary" @click="stopRecording">
+              End
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -72,17 +93,19 @@ export default {
       videoBlob: null,
       stream: null,
       recorder: null,
-      videoStates: {
-        UPLOAD: 0,
-        PLAYBACK: 1,
-        LIVE_PREVIEW: 2,
-        RECORD_STARTED: 3
+      states: {
+        UPLOAD: {
+          INIT: 0,
+          PLAYBACK: 1
+        },
+        RECORD: {
+          INIT: 2,
+          RECORDING: 3,
+          LIVE_PREVIEW: 4,
+          PLAYBACK: 5
+        }
       },
-      selectedStates: {
-        UPLOAD: 0,
-        RECORD: 1
-      },
-      videoState: 0,
+      state: 0,
       selectedState: 0,
       videoOptions: {
         controls: true,
@@ -95,7 +118,7 @@ export default {
     // window.videojs = videojs
     this.initVideoJs(this.$refs.videoPlayer)
     this.addFileInputListener()
-    this.videoState = this.videoStates.UPLOAD
+    this.state = this.states.UPLOAD.INIT
   },
   methods: {
     initVideoJs (el) {
@@ -113,7 +136,7 @@ export default {
     addFileInputListener () {
       this.$refs.fileInput.onchange = (event) => {
         if (event.target.files && event.target.files[0]) {
-          this.videoState = this.videoStates.PLAYBACK
+          this.state = this.states.UPLOAD.PLAYBACK
           this.videoBlob = event.target.files[0]
           this.setVideoJsSource(URL.createObjectURL(this.videoBlob), this.videoBlob.type)
         }
@@ -129,7 +152,7 @@ export default {
         this.recorder = RecordRTC(stream, {
           type: 'video'
         })
-        this.videoState = this.videoStates.LIVE_PREVIEW
+        this.state = this.states.RECORD.LIVE_PREVIEW
       }).catch((e) => {
         if (e.name === 'NotAllowedError') {
           // TODO: ask user to accept permission
@@ -139,7 +162,7 @@ export default {
     },
     startRecording () {
       this.recorder.startRecording()
-      this.videoState = this.videoStates.RECORD_STARTED
+      this.state = this.states.RECORD.RECORDING
     },
     stopRecording () {
       this.recorder.stopRecording(() => {
@@ -147,14 +170,20 @@ export default {
         this.videojsRef.src([
           { src: URL.createObjectURL(this.videoBlob), type: 'video/webm' }
         ])
-        this.videoState = this.videoStates.PLAYBACK
+        this.state = this.states.RECORD.PLAYBACK
       })
     },
-    isState (state, testState) {
+    triggerFileInput () {
+      this.$refs.fileInput.click()
+    },
+    isParentState (parent) {
+      return Object.values(this.states[parent]).some(state => state === this.state)
+    },
+    isState (testState) {
       if (Array.isArray(testState)) {
-        return Object.values(testState).some(s => s === state)
+        return Object.values(testState).some(s => s === this.state)
       } else {
-        return state === testState
+        return this.state === testState
       }
     }
   }
@@ -162,14 +191,37 @@ export default {
 </script>
 
 <style>
+
+.btn-active {
+  transform: translateY(5px);
+}
+.upload-zone {
+  height: 130px;
+  border: 1px solid #c7c7c7;
+  padding-top: 25px;
+  transition: 0.3s ease-in-out 0.5s;
+  transform-origin: top;
+}
+.upload-zone.shrink {
+  height: 0;
+  padding: 0;
+  border: none;
+}
+.upload-zone.shrink > p {
+  display: none;
+}
+.upload-btn {
+  margin-bottom: -190px;
+  transition: 0.3s ease-in-out 0.5s;
+}
+.upload-btn.chosen {
+  margin: 0;
+}
 .live-preview-video {
   width: 100%;
 }
 .video-method-btn {
   width: 200px;
   height: 50px;
-}
-.btn-active {
-  transform: translateY(5px);
 }
 </style>
