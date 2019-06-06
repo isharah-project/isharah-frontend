ceabc3ce9cc4f411404575a855fb230acc059b46
tarekalqaddy <tarek.alqaddy@gmail.com>
2019-06-06 16:16:04 +0200

    Remove rangeslider files, Adjust 404 page vertically

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container text-xs-center">
-    <div v-if="error.statusCode === 404">
-      <h1 class="status-code">
-        404
+    <div>
+      <div v-if="error.statusCode === 404">
+        <h1 class="status-code">
+          404
+        </h1>
+        <h1>الصفحة غير موجودة</h1>
+      </div>
+      <h1 v-else>
+        حدث خطأ ما, برجاء المحاولة مرة اخرى
       </h1>
-      <h1>الصفحة غير موجودة</h1>
+      <v-btn flat round class="white--text btn-shadow blue-cyan-gradient mt-3" @click="navigate">
+        {{ error.statusCode === 404 ? 'الرئيسية' : 'الرجوع' }}
+      </v-btn>
     </div>
-    <h1 v-else>
-      حدث خطأ ما, برجاء المحاولة مرة اخرى
-    </h1>
-    <v-btn flat round class="white--text btn-shadow blue-cyan-gradient mt-3" @click="navigate">
-      {{ error.statusCode === 404 ? 'الرئيسية' : 'الرجوع' }}
-    </v-btn>
   </div>
 </template>
 
@@ -37,7 +39,7 @@ export default {
 
 <style scoped>
 .container {
-  margin-top: 100px;
+  margin-top: 50px;
 }
 .status-code {
   font-size: 10rem;
