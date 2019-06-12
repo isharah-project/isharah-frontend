c391ba29e9b0e724b45d19bfb267cb04e14e691e
tarekalqaddy <tarek.alqaddy@gmail.com>
2019-06-12 12:58:56 +0200

    Add recording instructions in add gesture page

--- pages/contribute/add_gesture.vue
+++ pages/contribute/add_gesture.vue
@@ -8,21 +8,78 @@
       autoCompleteLabel="اختر الكلمة"
       :autoCompleteDeserializeResults="true"
     />
+    <v-dialog v-model="instructionsDialog" width="600" persistent>
+      <v-card class="medium-round-corners">
+        <v-card-title class="justify-center">
+          <h1>
+            التعليمات
+          </h1>
+        </v-card-title>
+        <v-card-text>
+          <ul class="px-4">
+            <li>
+              يفصل أن تكون الخلفية بيضاء أو فاتحة
+            </li>
+            <li>
+              لا تقم بأداء الإشارة بسرعة
+            </li>
+            <li>
+              يجب التصوير في ظروف إضاءة جيدة
+            </li>
+            <li>
+              يفضل الوقوف في منتصف الفيديو
+            </li>
+            <li>
+              بعد رفع الفيديو او التسجيل يمكنك اقتصاص الفيديو من البداية والنهاية عن طريق الأداة الموضحة بالأسفل والتي ستكون بأسفل الفيديو
+            </li>
+          </ul>
+          <img :src="VideoTrimmer" alt="video trimmer example" class="full-width btn-shadow mt-3">
+        </v-card-text>
+        <v-card-actions>
+          <v-checkbox v-model="dontShowInstructions" color="primary" label="لا تظهر التعليمات مرة أخرى" hide-details />
+        </v-card-actions>
+        <v-card-actions class="justify-center pt-0">
+          <v-btn class="blue-gradient fixed-size-btn btn-shadow" round dark @click="saveSettings">
+            موافق
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
 import PageHeader from '~/components/generic/PageHeader'
 import GestureUpload from '~/components/contribute/GestureUpload'
+import localStorage from 'local-storage'
+import VideoTrimmer from '~/assets/images/video-trimmer.png'
 
 export default {
   components: {
     PageHeader,
     GestureUpload
+  },
+  data () {
+    return {
+      VideoTrimmer,
+      instructionsDialog: true,
+      dontShowInstructions: false
+    }
+  },
+  created () {
+    this.instructionsDialog = !localStorage.get('addGesture.dontShowInstructions')
+  },
+  methods: {
+    saveSettings () {
+      localStorage.set('addGesture.dontShowInstructions', this.dontShowInstructions)
+      this.instructionsDialog = false
+    }
   }
 }
 </script>
 
 <style>
-
+.justify-center {
+  justify-content: center;
+}
 </style>
